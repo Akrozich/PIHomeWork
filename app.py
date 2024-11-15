446d8f5d01b5812dae29d911e9029a41daf7ea80
Akrozich <akrozijnebulov@gmail.com>
2024-11-16 03:19:01 +0700

    changed app.py script
    add division function

--- app.py
+++ app.py
@@ -7,9 +7,13 @@ def Proizvod(a,b):
 def Pow(a,b):
     return a**b
 
+def Division(a,b):
+    return a/b
+
 a = int(input("Enter a first number: "))
 b = int(input("Enter a second number: "))
 
 print(f"Sum:{Sum(a,b)}")
 print(f"Proizvod:{Proizvod(a,b)}")
 print(f"Pow:{Pow(a,b)}")
+print(f"Division:{Division(a,b)}")

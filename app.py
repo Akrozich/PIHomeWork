d10d1b107d56650d15ff257364b577f4e26d0d80
Akrozich <akrozijnebulov@gmail.com>
2024-11-16 03:15:17 +0700

    add app.py script
    app.py script includes a bases math functions

---
+++ app.py
@@ -0,0 +1,15 @@
+def Sum(a, b):
+    return a+b
+
+def Proizvod(a,b):
+    return a*b
+
+def Pow(a,b):
+    return a**b
+
+a = int(input("Enter a first number: "))
+b = int(input("Enter a second number: "))
+
+print(f"Sum:{Sum(a,b)}")
+print(f"Proizvod:{Proizvod(a,b)}")
+print(f"Pow:{Pow(a,b)}")
